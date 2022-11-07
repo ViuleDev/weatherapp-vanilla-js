e86f03d3f62fcd72f34c9ecfed11c5f57151436b
Vincenzo- <viulenzgg@gmail.com>
2022-11-07 12:23:40 +0100

    added max/min temp sub text

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ const displayWeather = (currentCityWeather) => {
   weatherIcon.src = `https://openweathermap.org/img/wn/${currentCityWeather.weather[0].icon}@2x.png`;
   weatherDescription.innerHTML = currentCityWeather.weather[0].description.toUpperCase();
   currentTemp.innerHTML = Math.trunc(currentCityWeather.main.temp) + "&#176;C";
-  minMaxTemp.innerHTML = `${Math.trunc(currentCityWeather.main.temp_max)}&#176;/${Math.trunc(currentCityWeather.main.temp_min)}&#176;`;
+  minMaxTemp.innerHTML = `${Math.trunc(currentCityWeather.main.temp_max)}&#176;<sub class="max">MAX</sub> ${Math.trunc(currentCityWeather.main.temp_min)}&#176;<sub class="min">MIN</sub>`;
   stateName.innerHTML = "";
 
   // Some item do not have a .state property, hence we use this guard close.
